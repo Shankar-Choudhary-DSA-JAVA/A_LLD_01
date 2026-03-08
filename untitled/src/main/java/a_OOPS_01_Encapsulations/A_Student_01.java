109d7980fb2c5f4bab17c00052dca11265d55e70
Shankar <shankarallen2003@gmail.com>
2026-03-08 17:10:56 +0530

    Day_01 Class LLD Finished

---
+++ untitled/src/main/java/a_OOPS_01_Encapsulations/A_Student_01.java
@@ -0,0 +1,23 @@
+package a_OOPS_01_Encapsulations;
+
+public class A_Student_01 {
+
+    String name;
+    int age;
+    String batch;
+    String address;
+    long mobile;
+
+
+    public void changeBatch(String batchName){
+//        this.batch=batchName; //or
+        System.out.println(this.batch=batchName);
+    }
+    public long changeMobile(){
+       return this.mobile=952141001L;
+    }
+
+
+
+
+}
